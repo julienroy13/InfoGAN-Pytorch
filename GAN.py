18f2248158a2eea5c47181b804660a87dd6f2475
Julien Roy <juroyd@cuda3.rdgi.polymtl.ca>
2018-04-30 23:07:56 -0400

    Commenting and simplifying GAN.py, part-2

--- GAN.py
+++ GAN.py
@@ -64,8 +64,9 @@ class generator(nn.Module):
 
     def initialize_weights(self):
         for module in self.modules():
-            module.weight.data.normal_(0, 0.02)
-            module.bias.data.zero_()
+            if isinstance(module, nn.Conv2d) or isinstance(module, nn.ConvTranspose2d) or isinstance(module, nn.Linear):
+                module.weight.data.normal_(0, 0.02)
+                module.bias.data.zero_()
 
 class discriminator(nn.Module):
     def __init__(self, dataset='mnist'):
@@ -119,8 +120,9 @@ class discriminator(nn.Module):
 
     def initialize_weights(self):
         for module in self.modules():
-            module.weight.data.normal_(0, 0.02)
-            module.bias.data.zero_()
+            if isinstance(module, nn.Conv2d) or isinstance(module, nn.ConvTranspose2d) or isinstance(module, nn.Linear):
+                module.weight.data.normal_(0, 0.02)
+                module.bias.data.zero_()
 
 
 class GAN(object):
@@ -132,7 +134,6 @@ class GAN(object):
         self.save_dir = args.save_dir
         self.result_dir = args.result_dir
         self.dataset = args.dataset
-        self.log_dir = args.log_dir
         self.gpu_mode = args.gpu_mode
         self.gpu_id = args.gpu_id
         self.model_name = args.gan_type
@@ -171,36 +172,49 @@ class GAN(object):
         else:
             self.sample_z_ = Variable(torch.rand((self.batch_size, self.z_dim)), volatile=True)
 
+        # Creates train history dictionnary to record important training indicators
+        self.train_history = {}
+        self.train_history['D_loss'] = []
+        self.train_history['G_loss'] = []
+        self.train_history['per_epoch_time'] = []
+        self.train_history['total_time'] = []
+
     def train(self):
-        self.train_hist = {}
-        self.train_hist['D_loss'] = []
-        self.train_hist['G_loss'] = []
-        self.train_hist['per_epoch_time'] = []
-        self.train_hist['total_time'] = []
 
+        # Creates artificial labels that just indicates to the loss object if prediction of D should be 0 or 1
         if self.gpu_mode:
-            self.y_real_, self.y_fake_ = Variable(torch.ones(self.batch_size, 1).cuda(self.gpu_id)), Variable(torch.zeros(self.batch_size, 1).cuda(self.gpu_id))
+            self.y_real_ = Variable(torch.ones(self.batch_size, 1).cuda(self.gpu_id)) # all ones
+            self.y_fake_ = Variable(torch.zeros(self.batch_size, 1).cuda(self.gpu_id)) # all zeros
         else:
-            self.y_real_, self.y_fake_ = Variable(torch.ones(self.batch_size, 1)), Variable(torch.zeros(self.batch_size, 1))
+            self.y_real_ =  Variable(torch.ones(self.batch_size, 1))
+            self.y_fake_ = Variable(torch.zeros(self.batch_size, 1))
 
-        self.D.train()
-        print('training start!!')
+        self.D.train() # sets discriminator in train mode
+        
+        # TRAINING LOOP
         start_time = time.time()
+        print('[*] TRAINING STARTS')
         for epoch in range(self.epoch):
-            self.G.train()
+            self.G.train() # sets generator in train mode
             epoch_start_time = time.time()
-            for iter, (x_, _) in enumerate(self.data_loader):
-                if iter == self.data_loader.dataset.__len__() // self.batch_size:
+            
+            # For each minibatch returned by the data_loader
+            for step, (x_, _) in enumerate(self.data_loader):
+                if step == self.data_loader.dataset.__len__() // self.batch_size:
                     break
 
+                # Creates a minibatch of latent vectors
                 z_ = torch.rand((self.batch_size, self.z_dim))
 
+                # Convert to Variables (sends to GPU if needed)
                 if self.gpu_mode:
-                    x_, z_ = Variable(x_.cuda(self.gpu_id)), Variable(z_.cuda(self.gpu_id))
+                    x_ = Variable(x_.cuda(self.gpu_id))
+                    z_ = Variable(z_.cuda(self.gpu_id))
                 else:
-                    x_, z_ = Variable(x_), Variable(z_)
+                    x_ = Variable(x_) 
+                    z_ = Variable(z_)
 
-                # update D network
+                # Updates D network
                 self.D_optimizer.zero_grad()
 
                 D_real = self.D(x_)
@@ -211,82 +225,60 @@ class GAN(object):
                 D_fake_loss = self.BCE_loss(D_fake, self.y_fake_)
 
                 D_loss = D_real_loss + D_fake_loss
-                self.train_hist['D_loss'].append(D_loss.data[0])
+                self.train_history['D_loss'].append(D_loss.data[0])
 
                 D_loss.backward()
                 self.D_optimizer.step()
 
-                # update G network
+                # Updates G network
                 self.G_optimizer.zero_grad()
 
                 G_ = self.G(z_)
                 D_fake = self.D(G_)
                 G_loss = self.BCE_loss(D_fake, self.y_real_)
-                self.train_hist['G_loss'].append(G_loss.data[0])
+                self.train_history['G_loss'].append(G_loss.data[0])
 
                 G_loss.backward()
                 self.G_optimizer.step()
 
-                if ((iter + 1) % 100) == 0:
-                    print("Epoch: [%2d] [%4d/%4d] D_loss: %.8f, G_loss: %.8f" %
-                          ((epoch + 1), (iter + 1), self.data_loader.dataset.__len__() // self.batch_size, D_loss.data[0], G_loss.data[0]))
+                # Prints training info every 100 steps
+                if ((step + 1) % 100) == 0:
+                    print("Epoch: [{:2d}] [{:4d}/{:4d}] D_loss: {:.8f}, G_loss: {:.8f}".format(
+                          (epoch + 1), (step + 1), self.data_loader.dataset.__len__() // self.batch_size, D_loss.data[0], G_loss.data[0]))
 
-            self.train_hist['per_epoch_time'].append(time.time() - epoch_start_time)
-            self.visualize_results((epoch+1))
+            self.train_history['per_epoch_time'].append(time.time() - epoch_start_time)
 
-        self.train_hist['total_time'].append(time.time() - start_time)
-        print("Avg one epoch time: %.2f, total %d epochs time: %.2f" % (np.mean(self.train_hist['per_epoch_time']),
-              self.epoch, self.train_hist['total_time'][0]))
-        print("Training finish!... save training results")
+        self.train_history['total_time'].append(time.time() - start_time)
+        print("Avg one epoch time: %.2f, total %d epochs time: %.2f" % (np.mean(self.train_history['per_epoch_time']),
+              self.epoch, self.train_history['total_time'][0]))
+        print("[*] TRAINING FINISHED")
 
+        # Saves the model
         self.save()
-        utils.generate_animation(self.result_dir + '/' + self.dataset + '/' + self.model_name + '/' + self.model_name,
-                                 self.epoch)
-        utils.loss_plot(self.train_hist, os.path.join(self.save_dir, self.dataset, self.model_name), self.model_name)
-
-    def visualize_results(self, epoch, fix=True):
-        self.G.eval()
-
-        if not os.path.exists(self.result_dir + '/' + self.dataset + '/' + self.model_name):
-            os.makedirs(self.result_dir + '/' + self.dataset + '/' + self.model_name)
-
-        tot_num_samples = min(self.sample_num, self.batch_size)
-        image_frame_dim = int(np.floor(np.sqrt(tot_num_samples)))
-
-        if fix:
-            """ fixed noise """
-            samples = self.G(self.sample_z_)
-        else:
-            """ random noise """
-            if self.gpu_mode:
-                sample_z_ = Variable(torch.rand((self.batch_size, self.z_dim)).cuda(self.gpu_id), volatile=True)
-            else:
-                sample_z_ = Variable(torch.rand((self.batch_size, self.z_dim)), volatile=True)
-
-            samples = self.G(sample_z_)
-
-        if self.gpu_mode:
-            samples = samples.cpu().data.numpy().transpose(0, 2, 3, 1)
-        else:
-            samples = samples.data.numpy().transpose(0, 2, 3, 1)
-
-        utils.save_images(samples[:image_frame_dim * image_frame_dim, :, :, :], [image_frame_dim, image_frame_dim],
-                          self.result_dir + '/' + self.dataset + '/' + self.model_name + '/' + self.model_name + '_epoch%03d' % epoch + '.png')
+        
+        # Saves the plot of losses for G and D
+        utils.save_loss_plot(self.train_history, filename=os.path.join(self.save_dir, self.dataset, self.model_name, 'curves'))
 
     def save(self):
+        # Defines save directory
         save_dir = os.path.join(self.save_dir, self.dataset, self.model_name)
 
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
+        # Saves the models
         torch.save(self.G.state_dict(), os.path.join(save_dir, self.model_name + '_G.pkl'))
         torch.save(self.D.state_dict(), os.path.join(save_dir, self.model_name + '_D.pkl'))
 
+        # Saves the train history
         with open(os.path.join(save_dir, self.model_name + '_history.pkl'), 'wb') as f:
-            pickle.dump(self.train_hist, f)
+            pickle.dump(self.train_history, f)
 
     def load(self):
+        # Defines save directory
         save_dir = os.path.join(self.save_dir, self.dataset, self.model_name)
 
+        # Loads the necessary models
         self.G.load_state_dict(torch.load(os.path.join(save_dir, self.model_name + '_G.pkl')))
-        if not self.test_only: self.D.load_state_dict(torch.load(os.path.join(save_dir, self.model_name + '_D.pkl')))
+        if not self.test_only: 
+            self.D.load_state_dict(torch.load(os.path.join(save_dir, self.model_name + '_D.pkl')))
