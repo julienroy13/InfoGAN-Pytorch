0f0d8882ab2c882c2be63ab82408bc5ad30f6cad
Julien Roy <juroyd@cuda3.rdgi.polymtl.ca>
2018-04-30 23:07:28 -0400

    Deleted every non-essential functions. EVERY remaining functions have been rewritten

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-# CODE FROM : github.com/znxlwm/pytorch-generative-model-collections
-
 import os
 import pickle
 import torch
@@ -7,6 +5,8 @@ import torch.nn as nn
 import numpy as np
 import scipy.misc
 import imageio
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
 import pdb
@@ -29,82 +29,38 @@ def load_mnist(dataset):
     Y = torch.from_numpy(Y).type(torch.FloatTensor)
     return X, Y
 
+# WE KEEP THIS FUNCTION (for now) AS A TEMPLATE FOR 3DCHAIRS AND SYNTH DATASETS--------
 def load_celebA(dir, transform, batch_size, shuffle):
-    # transform = transforms.Compose([
-    #     transforms.CenterCrop(160),
-    #     transform.Scale(64)
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    # ])
-
-    # data_dir = 'data/celebA'  # this path depends on your computer
     dset = datasets.ImageFolder(dir, transform)
     data_loader = torch.utils.data.DataLoader(dset, batch_size, shuffle)
 
     return data_loader
+# -----------------------------------------------------------------------------
 
 def print_network(net):
     # Counts the number of parameters
     num_params = 0
     for param in net.parameters():
         num_params += param.numel()
+
+    # Prints the architecture
     print('\nNETWORK ARCHITECTURE ------------------------')
     print(net)
-    print('Total number of parameters : {}k}'.format(int(num_params/1e3)))
+    print('Total number of parameters : {}k'.format(int(num_params/1e3)))
     print('-----------------------------------------------\n')
 
-def save_images(images, size, image_path):
-    return imsave(images, size, image_path)
+def save_loss_plot(train_history, filename):
+    plt.figure(figsize=(10,4))
 
-def imsave(images, size, path):
-    image = np.squeeze(merge(images, size))
-    return scipy.misc.imsave(path, image)
-
-def merge(images, size):
-    h, w = images.shape[1], images.shape[2]
-    if (images.shape[3] in (3,4)):
-        c = images.shape[3]
-        img = np.zeros((h * size[0], w * size[1], c))
-        for idx, image in enumerate(images):
-            i = idx % size[1]
-            j = idx // size[1]
-            img[j * h:j * h + h, i * w:i * w + w, :] = image
-        return img
-    elif images.shape[3]==1:
-        img = np.zeros((h * size[0], w * size[1]))
-        for idx, image in enumerate(images):
-            i = idx % size[1]
-            j = idx // size[1]
-            img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
-        return img
-    else:
-        raise ValueError('in merge(images,size) images parameter ''must have dimensions: HxW or HxWx3 or HxWx4')
-
-def generate_animation(path, num):
-    images = []
-    for e in range(num):
-        img_name = path + '_epoch%03d' % (e+1) + '.png'
-        images.append(imageio.imread(img_name))
-    imageio.mimsave(path + '_generate_animation.gif', images, fps=5)
-
-def loss_plot(hist, path = 'Train_hist.png', model_name = ''):
-    x = range(len(hist['D_loss']))
-
-    y1 = hist['D_loss']
-    y2 = hist['G_loss']
-
-    plt.plot(x, y1, label='D_loss')
-    plt.plot(x, y2, label='G_loss')
-
-    plt.xlabel('Iter')
+    # Defines the plot
+    plt.plot(train_history['D_loss'], color="blue", label='D loss')
+    plt.plot(train_history['G_loss'], color="orange", label='G loss')
+    plt.title('Training Curves', fontweight='bold')
+    plt.xlabel('Training steps')
     plt.ylabel('Loss')
+    plt.legend(loc='best')
+    plt.grid(True, color="lightgrey")
 
-    plt.legend(loc=4)
-    plt.grid(True)
-    plt.tight_layout()
-
-    path = os.path.join(path, model_name + '_loss.png')
-
-    plt.savefig(path)
-
+    # Saves the figure
+    plt.savefig(filename)
     plt.close()
