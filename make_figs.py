49e5734e09667f8441b7327a54ac0fcc55e9e00c
Julien Roy <juroyd@cuda3.rdgi.polymtl.ca>
2018-05-06 11:07:35 -0400

    Added code for figure 2

--- make_figs.py
+++ make_figs.py
@@ -34,36 +34,21 @@ def parse_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    # parse arguments
-    args = parse_args()
-
-    # instanciates the model
-    model = MODEL(args)
-
-    # trains the model
-    train(model)
-
-
-
-
-
-
-
 # ===================================
 # The folowing functions are not used during training.
 # Only used to generate figures by running make_figs.py directly
 
 def generate_figure1(model, filename):
+    """Shows the effect of varying the discrete codes"""
     model.G.eval()
 
     # Creates samples and saves them
     plt.figure(figsize=(20,8))
 
-    z_dim = 62
+    z_dim = model.z_dim
     n_samples = 5
-    c_disc_dim = 10
-    c_cont_dim = 2
+    c_disc_dim = model.c_disc_dim
+    c_cont_dim = model.c_cont_dim
     k=1
     for i in range(n_samples):
 
@@ -81,11 +66,11 @@ def generate_figure1(model, filename):
             z = Variable(z, volatile=True)
             c_cont = Variable(c_cont, volatile=True)
 
-        for j in range(n_disc_codes):
+        for j in range(c_disc_dim):
             
             # Discrete code
-            c_disc = torch.from_numpy(np.zeros(shape=(1,n_disc_codes))).type(torch.FloatTensor)
-            c_disc[j] = 1.
+            c_disc = torch.from_numpy(np.zeros(shape=(1,c_disc_dim))).type(torch.FloatTensor)
+            c_disc[0, j] = 1.
 
             # Converts to Variable (sends to GPU if necessary)
             if model.gpu_mode:
@@ -108,12 +93,88 @@ def generate_figure1(model, filename):
     plt.savefig(filename, bbox_inches='tight')
     plt.close()
 
-def generate_figure2(model, filename):
-    pass
+def generate_figure2(model, filename, c_idx):
+    """Shows the effect of varying a continuous code (c_idx)"""
+    model.G.eval()
+
+    # Creates samples and saves them
+    plt.figure(figsize=(20,8))
+
+    z_dim = model.z_dim
+    n_samples = 5
+    c_disc_dim = model.c_disc_dim
+    c_cont_dim = model.c_cont_dim
+    k=1
+    for i in range(n_samples):
+
+        # Basic z vector
+        z = torch.rand((1, z_dim))
+
+        # Discrete code
+        c_disc = torch.from_numpy(np.random.multinomial(1, model.c_disc_dim * [float(1.0 / model.c_disc_dim)], size=[1])).type(torch.FloatTensor)
+        for j in range(model.n_disc_code-1):
+            c_disc = torch.cat([c_disc, torch.from_numpy(np.random.multinomial(1, model.c_disc_dim * [float(1.0 / model.c_disc_dim)], size=[1])).type(torch.FloatTensor)], dim=1)
+
+        # Converts to Variable (sends to GPU if necessary)
+        if model.gpu_mode:
+            z = Variable(z.cuda(model.gpu_id), volatile=True)
+            c_disc = Variable(c_disc.cuda(model.gpu_id), volatile=True)
+        else:
+            z = Variable(z, volatile=True)
+            c_disc = Variable(c_disc, volatile=True)
+
+        for j in range(10):
+            
+            # Continuous code
+            c_cont = torch.from_numpy(np.array([[0., 0.]])).type(torch.FloatTensor)
+            c_cont[0, c_idx] = -1. + (0.2 * j)
+
+            # Converts to Variable (sends to GPU if necessary)
+            if model.gpu_mode:
+                c_cont = Variable(c_cont.cuda(model.gpu_id), volatile=True)
+            else:
+                c_cont = Variable(c_cont, volatile=True)
+
+            # Forward propagation
+            x = model.G(z, c_cont, c_disc, model.dataset)
+
+            # Reshapes dimensions and convert to ndarray
+            x = x.cpu().data.numpy().transpose(0, 2, 3, 1).squeeze()
+
+            # Adds sample to the figure
+            plt.subplot(n_samples, 10, k)
+            plt.imshow(x, cmap='gray')
+            plt.axis('off')
+            k += 1
+
+    plt.savefig(filename, bbox_inches='tight')
+    plt.close()
 
 def generate_figure3(model, filename):
     pass
 
-if __name__ == "__main__":
 
-    pass
+
+if __name__ == '__main__':
+    # parse arguments
+    args = parse_args()
+
+    # instanciates the model
+    model = MODEL(args, test_only=True)
+
+    # trains the model
+    model.load()
+
+    # creates the figure directory
+    file_dir = os.path.join("figures", model.dataset, model.model_type)
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+
+    # sets the random seed for reproducibility
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    # generates the figures
+    generate_figure1(model, filename=os.path.join(file_dir, "figure1.png"))
+    generate_figure2(model, filename=os.path.join(file_dir, "figure2_c0.png"), c_idx=0)
+    generate_figure2(model, filename=os.path.join(file_dir, "figure2_c1.png"), c_idx=1)
